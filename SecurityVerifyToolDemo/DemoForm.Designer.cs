4e4540763ed8f8512dd8da537b7b3891e7ae9f75
fgultekin@pbt.com.tr <fgultekin@pbt.com.tr>
2015-07-20 15:22:39 +0300

    first commit

---
+++ SecurityVerifyToolDemo/DemoForm.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Tr.Com.Panaroma.Ngcrd.SecurityVerifyTool
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInSModeId = new System.Windows.Forms.TextBox();
+            this.txtInServiceId = new System.Windows.Forms.TextBox();
+            this.txtInFiscalNum = new System.Windows.Forms.TextBox();
+            this.txtOutVerificationKey = new System.Windows.Forms.TextBox();
+            this.txtOutServiceId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOutFiscalCode = new System.Windows.Forms.TextBox();
+            this.txtOutFiscalNum = new System.Windows.Forms.TextBox();
+            this.txtOutDllMessage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnGenerateVerifyKey = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutDllRetCode = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInSModeId
+            // 
+            this.txtInSModeId.Location = new System.Drawing.Point(6, 19);
+            this.txtInSModeId.Name = "txtInSModeId";
+            this.txtInSModeId.Size = new System.Drawing.Size(146, 20);
+            this.txtInSModeId.TabIndex = 0;
+            this.txtInSModeId.Text = "631B9D144901D71A";
+            // 
+            // txtInServiceId
+            // 
+            this.txtInServiceId.Location = new System.Drawing.Point(6, 45);
+            this.txtInServiceId.Name = "txtInServiceId";
+            this.txtInServiceId.Size = new System.Drawing.Size(146, 20);
+            this.txtInServiceId.TabIndex = 1;
+            this.txtInServiceId.Text = "12345";
+            // 
+            // txtInFiscalNum
+            // 
+            this.txtInFiscalNum.Location = new System.Drawing.Point(6, 71);
+            this.txtInFiscalNum.Name = "txtInFiscalNum";
+            this.txtInFiscalNum.Size = new System.Drawing.Size(145, 20);
+            this.txtInFiscalNum.TabIndex = 2;
+            this.txtInFiscalNum.Text = "12345678";
+            // 
+            // txtOutVerificationKey
+            // 
+            this.txtOutVerificationKey.Location = new System.Drawing.Point(6, 23);
+            this.txtOutVerificationKey.Name = "txtOutVerificationKey";
+            this.txtOutVerificationKey.Size = new System.Drawing.Size(100, 20);
+            this.txtOutVerificationKey.TabIndex = 3;
+            // 
+            // txtOutServiceId
+            // 
+            this.txtOutServiceId.Location = new System.Drawing.Point(6, 49);
+            this.txtOutServiceId.Name = "txtOutServiceId";
+            this.txtOutServiceId.Size = new System.Drawing.Size(100, 20);
+            this.txtOutServiceId.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "S Mode ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Service Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fiscal Num";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Verification Key";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(112, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Service Id";
+            // 
+            // txtOutFiscalCode
+            // 
+            this.txtOutFiscalCode.Location = new System.Drawing.Point(222, 23);
+            this.txtOutFiscalCode.Name = "txtOutFiscalCode";
+            this.txtOutFiscalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtOutFiscalCode.TabIndex = 10;
+            // 
+            // txtOutFiscalNum
+            // 
+            this.txtOutFiscalNum.Location = new System.Drawing.Point(222, 49);
+            this.txtOutFiscalNum.Name = "txtOutFiscalNum";
+            this.txtOutFiscalNum.Size = new System.Drawing.Size(100, 20);
+            this.txtOutFiscalNum.TabIndex = 11;
+            // 
+            // txtOutDllMessage
+            // 
+            this.txtOutDllMessage.Location = new System.Drawing.Point(6, 102);
+            this.txtOutDllMessage.Name = "txtOutDllMessage";
+            this.txtOutDllMessage.Size = new System.Drawing.Size(316, 20);
+            this.txtOutDllMessage.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(329, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Fiscal Code";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(329, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Fiscal Num";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(329, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Dll Message";
+            // 
+            // btnGenerateVerifyKey
+            // 
+            this.btnGenerateVerifyKey.Location = new System.Drawing.Point(228, 19);
+            this.btnGenerateVerifyKey.Name = "btnGenerateVerifyKey";
+            this.btnGenerateVerifyKey.Size = new System.Drawing.Size(166, 72);
+            this.btnGenerateVerifyKey.TabIndex = 16;
+            this.btnGenerateVerifyKey.Text = "Generate Verification Key";
+            this.btnGenerateVerifyKey.UseVisualStyleBackColor = true;
+            this.btnGenerateVerifyKey.Click += new System.EventHandler(this.btnGenerateVerifyKey_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtOutDllRetCode);
+            this.groupBox1.Controls.Add(this.txtOutDllMessage);
+            this.groupBox1.Controls.Add(this.txtOutVerificationKey);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtOutServiceId);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtOutFiscalCode);
+            this.groupBox1.Controls.Add(this.txtOutFiscalNum);
+            this.groupBox1.Location = new System.Drawing.Point(12, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 144);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output Fields";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGenerateVerifyKey);
+            this.groupBox2.Controls.Add(this.txtInSModeId);
+            this.groupBox2.Controls.Add(this.txtInServiceId);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtInFiscalNum);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(409, 107);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input Fields";
+            // 
+            // txtOutDllRetCode
+            // 
+            this.txtOutDllRetCode.Location = new System.Drawing.Point(7, 76);
+            this.txtOutDllRetCode.Name = "txtOutDllRetCode";
+            this.txtOutDllRetCode.Size = new System.Drawing.Size(315, 20);
+            this.txtOutDllRetCode.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(329, 79);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Dll Rt Code";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 285);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Security Verify Tool";
+            this.Load += new System.EventHandler(this.DemoForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInSModeId;
+        private System.Windows.Forms.TextBox txtInServiceId;
+        private System.Windows.Forms.TextBox txtInFiscalNum;
+        private System.Windows.Forms.TextBox txtOutVerificationKey;
+        private System.Windows.Forms.TextBox txtOutServiceId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOutFiscalCode;
+        private System.Windows.Forms.TextBox txtOutFiscalNum;
+        private System.Windows.Forms.TextBox txtOutDllMessage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnGenerateVerifyKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtOutDllRetCode;
+    }
+}
+
---
+++ SecurityVerifyToolDemo/DemoForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tr.Com.Panaroma.Ngcrd.SecurityVerifyTool
+{
+    /// <summary>
+    /// This form describes how to use SecurityVerifyTool library
+    /// </summary>
+    public partial class DemoForm : Form
+    {
+        public DemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DemoForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerateVerifyKey_Click(object sender, EventArgs e)
+        {
+            string verificationKey;
+            string fiscalCode;
+            string fiscalNum;
+            string serviceId;
+            string dllDescription;
+
+            int dllRetCode = SecurityVerifier.GenerateVerifyKey(
+                txtInSModeId.Text, 
+                txtInFiscalNum.Text, 
+                txtInServiceId.Text,
+                out verificationKey,
+                out fiscalCode,
+                out fiscalNum,
+                out serviceId,
+                out dllDescription);
+
+            txtOutVerificationKey.Text = verificationKey;
+            txtOutFiscalCode.Text = fiscalCode;
+            txtOutFiscalNum.Text = fiscalNum;
+            txtOutServiceId.Text = serviceId;
+            txtOutDllMessage.Text = dllDescription;
+            txtOutDllRetCode.Text = Convert.ToString(dllRetCode);
+        }
+    }
+}
